06efa26d6ffd1db3cee1f6f054794828d914e0d8
Yira01 <yirasteysimc@outlook.com>
2024-07-29 19:22:04 -0400

    Registro de incidencias y visualización de incidencias

---
+++ lib/screens/incident_detail_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'dart:io';
+import 'package:tecnicos_minerd/models/incident_model.dart';
+
+class IncidentDetailScreen extends StatelessWidget {
+  final Incident incident;
+
+  IncidentDetailScreen({required this.incident});
+
+  @override
+  Widget build(BuildContext context) {
+    final player = AudioPlayer();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Detalles de la Incidencia'),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Título: ${incident.title}', style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            const SizedBox(height: 10),
+            Text('Centro Educativo: ${incident.schoolName}', style: const TextStyle(fontSize: 16)),
+            const SizedBox(height: 10),
+            Text('Regional: ${incident.regional}', style: const TextStyle(fontSize: 16)),
+            const SizedBox(height: 10),
+            Text('Distrito: ${incident.district}', style: const TextStyle(fontSize: 16)),
+            const SizedBox(height: 10),
+            Text('Fecha: ${incident.date}', style: const TextStyle(fontSize: 16)),
+            const SizedBox(height: 10),
+            Text('Descripción: ${incident.description}', style: const TextStyle(fontSize: 16)),
+            const SizedBox(height: 20),
+            if (incident.photo != null && incident.photo!.isNotEmpty)
+              Image.file(File(incident.photo!)),
+            const SizedBox(height: 20),
+            if (incident.audio != null && incident.audio!.isNotEmpty)
+              ElevatedButton(
+                onPressed: () async {
+                  await player.setSourceDeviceFile(incident.audio!);
+                  player.resume();
+                },
+                child: const Text('Reproducir Audio'),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
